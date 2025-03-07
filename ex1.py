3568f6917e678f0cf542b64bba2e06439b347046
cecimendesop <cecilia.mopaulo@gmail.com>
2025-03-07 14:54:03 -0300

    ex1 poo

--- ex1.py
+++ ex1.py
@@ -1,26 +1,38 @@
 class Veiculo:
     def __init__(self, marca, modelo, ano, ligado=False):
-        self.marca = marca
+        self.__marca = marca
         self.modelo = modelo
         self.ano = ano
-        self.ligado = ligado
+        self._ligado = ligado
+
+    def get_marca(self):
+        return self.__marca
+    def set_marca(self, marca):
+        if not self.__marca:
+            self.__marca = marca
+            print(f"A marca da moto é: {self.__marca}")
+        else:
+            print(f"Insira o marca da moto: {self.__marca}")
+
+    def get_ligado(self):
+        return self._ligado
 
     def apresentar(self):
         print("\n")
         print("-" * 20)
-        print(f"Marca: {self.marca}, Modelo: {self.modelo}, Ano: {self.ano}")
-        print("Ligado" if self.ligado else "Desligado")
+        print(f"Marca: {self.__marca}, Modelo: {self.modelo}, Ano: {self.ano}")
+        print("Ligado" if self._ligado else "Desligado")
 
-    def ligar(self):
-        if not self.ligado:
-            self.ligado = True
+    def set_ligar(self):
+        if not self._ligado:
+            self._ligado = True
             print(f"{self.modelo} foi ligado.")
         else:
             print(f"{self.modelo} já está ligado.")
 
-    def desligar(self):
-        if self.ligado:
-            self.ligado = False
+    def set_desligar(self):
+        if self._ligado:
+            self._ligado = False
             print(f"{self.modelo} foi desligado.")
         else:
             print(f"{self.modelo} já está desligado.")
@@ -32,7 +44,7 @@ class Moto(Veiculo):
         self.cilindradas = cilindradas
 
     def empinar(self):
-        if self.ligado:
+        if self._ligado:
             print(f"{self.modelo} está empinando!")
         else:
             print(f"{self.modelo} precisa estar ligado para empinar.")
@@ -44,13 +56,13 @@ class Moto(Veiculo):
 
 veiculo1 = Veiculo("Toyota", "Corolla", 2022)
 veiculo1.apresentar()
-veiculo1.ligar()
+veiculo1.set_ligar()
 veiculo1.apresentar()
-veiculo1.desligar()
+veiculo1.set_desligar()
 
 moto1 = Moto("Honda", "CBR 600RR", 2021, "600cc")
 moto1.apresentar()
-moto1.ligar()
+moto1.set_ligar()
 moto1.empinar()
-moto1.desligar()
+moto1.set_desligar()
 
